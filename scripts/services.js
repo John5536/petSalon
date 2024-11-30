56103d647df50af23274097fe3ba01ec82b59c63
John5536 <rrivers52@gmail.com>
2024-11-29 21:28:27 -0500

    FSDI-105-assignment3

--- scripts/services.js
+++ scripts/services.js
@@ -1,52 +1,66 @@
-class salonServices {
-  constructor(userName, petName, serviceNeeded, appointmentDate) {
-      this.userName = userName;
-      this.petName = petName;
-      this.serviceNeeded = serviceNeeded;
-      this.appointmentDate = appointmentDate;
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+  class salonServices {
+      constructor(userName, petName, serviceNeeded, appointmentDate) {
+          this.userName = userName;
+          this.petName = petName;
+          this.serviceNeeded = serviceNeeded;
+          this.appointmentDate = appointmentDate;
+      }
   }
-}
 
-let servicesChosen = JSON.parse(localStorage.getItem('servicekey')) || []; 
+  
+  let servicesChosen = JSON.parse(localStorage.getItem('servicekey')) || []; 
 
-function servicesManager() {
-  let personName = document.getElementById("userName").value;
-  let animalName = document.getElementById("animalNameindex").value;
-  let serviceName = document.getElementById("serviceDesired").value;
-  let appointmentDate = document.getElementById("aptDate").value;
+  
+  function servicesManager() {
+      let personName = document.getElementById("userName").value;
+      let animalName = document.getElementById("animalNameindex").value;
+      let serviceName = document.getElementById("serviceDesired").value;
+      let appointmentDate = document.getElementById("aptDate").value;
 
-  let newServiceslist = new salonServices(personName, animalName, serviceName, appointmentDate);
+      
+      let newServiceslist = new salonServices(personName, animalName, serviceName, appointmentDate);
 
-  servicesChosen.push(newServiceslist);
+      
+      servicesChosen.push(newServiceslist);
 
-  let servicesString = JSON.stringify(servicesChosen);
+      
+      let servicesString = JSON.stringify(servicesChosen);
+      localStorage.setItem('servicekey', servicesString);
 
-  localStorage.setItem('servicekey', servicesString);
+      
+      console.log(servicesChosen);
 
-  console.log(servicesChosen);
+      
+      displayInhtml();
+  }
 
-  displayInhtml(); 
-}
+  
+  let serviceButton = document.getElementById("servicesBtn");
+  if (serviceButton) {
+      serviceButton.addEventListener("click", function() {
+          servicesManager(); 
+      });
+  }
 
-let serviceButton = document.getElementById("servicesBtn");
+  
+  function displayInhtml() {
+      
+      let storedServices = localStorage.getItem('servicekey');
+      let parsedinfo = JSON.parse(storedServices);
+      let servicesDisplay2 = document.getElementById("services-Selected-register2");
 
-serviceButton.addEventListener("click", function() {
-  servicesManager(); 
+      
+      servicesDisplay2.innerHTML = '';
+
+      
+      parsedinfo.forEach(service => {
+          servicesDisplay2.innerHTML += `User: ${service.userName}, Pet: ${service.petName}, Service: ${service.serviceNeeded}, Date: ${service.appointmentDate}<br>`;
+      });
+  }
+
+  
+  displayInhtml();
 });
-
-function displayInhtml() {
-  let storedServices = localStorage.getItem('servicekey');
-  let parsedinfo = JSON.parse(storedServices);
-  let servicesDisplay2 = document.getElementById("servicesDisplay");
-
-  
-  servicesDisplay2.innerHTML = '';
-
-  
-  parsedinfo.forEach(service => {
-      servicesDisplay2.innerHTML += `User: ${service.userName}, Pet: ${service.petName}, Service: ${service.serviceNeeded}, Date: ${service.appointmentDate}<br>`;
-  });
-}
-
-
-displayInhtml();
